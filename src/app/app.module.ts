f4f96c96a8d8ffc5ac0f938cd8efbdd3b3d95e7b
VCalosch <carlos.villanueva@alumnos.uneatlantico.es>
2017-10-30 09:36:16 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+//Rutas
+import {app_routing} from './app.routes';
+
+//Componentes
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { CoordinadorComponent } from './components/coordinador/coordinador.component';
+import { SocorristaComponent } from './components/socorrista/socorrista.component';
+import { AdminGeneralComponent } from './components/admin-general/admin-general.component';
+import { GestorPlayasComponent } from './components/gestor-playas/gestor-playas.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    CoordinadorComponent,
+    SocorristaComponent,
+    AdminGeneralComponent,
+    GestorPlayasComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    app_routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
